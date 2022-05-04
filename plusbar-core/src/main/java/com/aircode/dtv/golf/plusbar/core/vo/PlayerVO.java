44eac5cf87d114b1b090bc4e6d023f5b5c430721
김성근 <sgkim@aircode.com>
2022-05-04 14:18:37 +0900

    VO 파라미터 변경

--- plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/vo/PlayerVO.java
+++ plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/vo/PlayerVO.java
@@ -13,13 +13,6 @@ public class PlayerVO {
 	private String playerName;
 	private String playerEnglishName;   // "",
 	private String playerImageUrl;   // "",
-	private String bitrhDate;   // "",
-	private String firstDebut;   // 최초 입회,
-	private String tourDebut;   // 투어프로 입회,
-	private String koreanTourDebut;   // 코리안투어 데뷔,
-	private String contract;   // "",
-	private String affiliatedTeam;   // "",
-	
 	
 	public String getGameId() {
 		return gameId;
@@ -51,48 +44,11 @@ public class PlayerVO {
 	public void setPlayerImageUrl(String playerImageUrl) {
 		this.playerImageUrl = playerImageUrl;
 	}
-	public String getBitrhDate() {
-		return bitrhDate;
-	}
-	public void setBitrhDate(String bitrhDate) {
-		this.bitrhDate = bitrhDate;
-	}
-	public String getFirstDebut() {
-		return firstDebut;
-	}
-	public void setFirstDebut(String firstDebut) {
-		this.firstDebut = firstDebut;
-	}
-	public String getTourDebut() {
-		return tourDebut;
-	}
-	public void setTourDebut(String tourDebut) {
-		this.tourDebut = tourDebut;
-	}
-	public String getKoreanTourDebut() {
-		return koreanTourDebut;
-	}
-	public void setKoreanTourDebut(String koreanTourDebut) {
-		this.koreanTourDebut = koreanTourDebut;
-	}
-	public String getContract() {
-		return contract;
-	}
-	public void setContract(String contract) {
-		this.contract = contract;
-	}
-	public String getAffiliatedTeam() {
-		return affiliatedTeam;
-	}
-	public void setAffiliatedTeam(String affiliatedTeam) {
-		this.affiliatedTeam = affiliatedTeam;
-	}
 	@Override
 	public String toString() {
 		return "PlayerVO [gameId=" + gameId + ", playerId=" + playerId + ", playerName=" + playerName
-				+ ", playerEnglishName=" + playerEnglishName + ", playerImageUrl=" + playerImageUrl + ", bitrhDate="
-				+ bitrhDate + ", firstDebut=" + firstDebut + ", tourDebut=" + tourDebut + ", koreanTourDebut="
-				+ koreanTourDebut + ", contract=" + contract + ", affiliatedTeam=" + affiliatedTeam + "]";
+				+ ", playerEnglishName=" + playerEnglishName + ", playerImageUrl=" + playerImageUrl + "]";
 	}
+
 	
 }
