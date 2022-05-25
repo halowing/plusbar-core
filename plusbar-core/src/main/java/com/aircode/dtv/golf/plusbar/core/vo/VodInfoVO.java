b51e1b55692b9f64a13c9960046a08116824ad7a
"sgkim@thecnp.co.kr" <"sgkim@thecnp.co.kr">
2022-05-25 19:51:08 +0900

    VOD VO 추가 및 registDatetime 추가

--- plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/vo/VodInfoVO.java
+++ plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/vo/VodInfoVO.java
@@ -1,67 +1,38 @@
-package com.aircode.dtv.golf.plusbar.core.dto;
+package com.aircode.dtv.golf.plusbar.core.vo;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-
 import com.aircode.dtv.golf.plusbar.core.code.VideoResolution;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class VodInfoDTO implements Serializable {
+public class VodInfoVO {
 
-	private static final long serialVersionUID = -7855570639626855914L;
 	
 	private String vodId;
-	
-	@NotBlank
 	private String gameId;
-	
-	@NotBlank
 	private String playerId;
-	
-	@Max(value = 4)
-	@Min(value = 1)
 	private Integer roundIdx;
-	
-	@NotNull
-	@Past
 	private LocalDateTime produceDatetime;
-	
-	private LocalDateTime registDatetime;
-	
-	@NotBlank
 	private String cdnId;
-	
-	@NotBlank
 	private String playUri;
-	
 	private String playerName;
 	private String title;
 	private String description;
 	private String posterUri;
 	private VideoResolution resolution;
 	private Long playTime;
-	
-	@NotNull
+	private LocalDateTime registDatetime;
 	private Boolean isRoundHighlight;
-	
-	@NotNull
 	private Boolean hasAd;
 	
-	
-	public VodInfoDTO() {}
-	
+	@Override
+	public String toString() {
+		return "VodInfoVO [vodId=" + vodId + ", gameId=" + gameId + ", playerId=" + playerId + ", roundIdx=" + roundIdx
+				+ ", produceDatetime=" + produceDatetime + ", cdnId=" + cdnId + ", playUri=" + playUri + ", playerName="
+				+ playerName + ", title=" + title + ", description=" + description + ", posterUri=" + posterUri
+				+ ", resolution=" + resolution + ", playTime=" + playTime + ", registDatetime=" + registDatetime
+				+ ", isRoundHighlight=" + isRoundHighlight + ", hasAd=" + hasAd + "]";
+	}
+
 	public String getVodId() {
 		return vodId;
 	}
@@ -166,6 +137,14 @@ public class VodInfoDTO implements Serializable {
 		this.playTime = playTime;
 	}
 
+	public LocalDateTime getRegistDatetime() {
+		return registDatetime;
+	}
+
+	public void setRegistDatetime(LocalDateTime registDatetime) {
+		this.registDatetime = registDatetime;
+	}
+
 	public Boolean getIsRoundHighlight() {
 		return isRoundHighlight;
 	}
@@ -181,24 +160,6 @@ public class VodInfoDTO implements Serializable {
 	public void setHasAd(Boolean hasAd) {
 		this.hasAd = hasAd;
 	}
-
-	@Override
-	public String toString() {
-		return "VodInfoDTO [vodId=" + vodId + ", gameId=" + gameId + ", playerId=" + playerId + ", roundIdx=" + roundIdx
-				+ ", produceDatetime=" + produceDatetime + ", registDatetime=" + registDatetime + ", cdnId=" + cdnId
-				+ ", playUri=" + playUri + ", playerName=" + playerName + ", title=" + title + ", description="
-				+ description + ", posterUri=" + posterUri + ", resolution=" + resolution + ", playTime=" + playTime
-				+ ", isRoundHighlight=" + isRoundHighlight + ", hasAd=" + hasAd + "]";
-	}
-
-	public LocalDateTime getRegistDatetime() {
-		return registDatetime;
-	}
-
-	public void setRegistDatetime(LocalDateTime registDatetime) {
-		this.registDatetime = registDatetime;
-	}
-
 	
 	
 }
