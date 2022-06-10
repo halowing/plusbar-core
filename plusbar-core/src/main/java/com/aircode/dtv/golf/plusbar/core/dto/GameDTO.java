cea2e7e005b04e800fa7c4d51c71f004db6b9605
김성근 <sgkim@aircode.com>
2022-06-10 21:32:23 +0900

    Admin에서 gameCloseDate 와 gameOpenDate 가 yyyy-MM-dd 형태로 쓰임.

--- plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/dto/GameDTO.java
+++ plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/dto/GameDTO.java
@@ -1,12 +1,15 @@
 package com.aircode.dtv.golf.plusbar.core.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import com.aircode.dtv.golf.plusbar.core.vo.GameVO;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.halowing.util.StringUtility;
+import com.halowing.util.TimeUtility;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
@@ -19,13 +22,22 @@ public class GameDTO extends GameVO implements Serializable {
 	
 	public GameDTO(GameVO vo) {
 		setAmtUnit(vo.getAmtUnit());
-		setGameCloseDate(vo.getGameCloseDate());
 		setGameId(vo.getGameId());
 		setGameName(vo.getGameName());
-		setGameOpenDate(vo.getGameOpenDate());
 		setGamePlayerCnt(vo.getGamePlayerCnt());
 		setGamePrize(vo.getGamePrize());
 		setGameRoundCnt(vo.getGameRoundCnt());
 		setSeriesId(vo.getSeriesId());
+		
+		if(StringUtility.isBlank(vo.getGameCloseDate()))
+			setGameCloseDate(null);
+		else 
+			setGameCloseDate(LocalDate.parse(vo.getGameCloseDate(),TimeUtility.DB_DATE_FORMATTER).format(TimeUtility.JSON_DATE_FORMATTER)  );
+		
+		if(StringUtility.isBlank(vo.getGameOpenDate()))
+			setGameOpenDate(null);
+		else 
+			setGameOpenDate(LocalDate.parse(vo.getGameOpenDate(),TimeUtility.DB_DATE_FORMATTER).format(TimeUtility.JSON_DATE_FORMATTER)  );
+		
 	}
 }
