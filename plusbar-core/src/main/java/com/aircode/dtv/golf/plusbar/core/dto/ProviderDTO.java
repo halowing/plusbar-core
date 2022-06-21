9b78b3958a3013aea08f101106a8b22a8ba6c79f
김성근 <sgkim@aircode.com>
2022-06-21 12:38:45 +0900

    ProviderDTO에서ㅓ ProviderVO extends를 삭제 validate 위해

--- plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/dto/ProviderDTO.java
+++ plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/dto/ProviderDTO.java
@@ -2,6 +2,8 @@ package com.aircode.dtv.golf.plusbar.core.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
+
 import com.aircode.dtv.golf.plusbar.core.vo.ProviderVO;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -11,7 +13,15 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.ALWAYS)
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class ProviderDTO extends ProviderVO implements Serializable {
+public class ProviderDTO implements Serializable {
+	
+	@NotBlank(message = "접속자 ID는 필수 정보입니다.")
+	private String providerId;
+	
+	@NotBlank(message = "접속자 명은 필수 정보 입니다.")
+	private String providerName;
+	
+	private String apiKey;
 
 	private static final long serialVersionUID = -5298302214692445025L;
 	
@@ -21,4 +31,28 @@ public class ProviderDTO extends ProviderVO implements Serializable {
 		setProviderName(vo.getProviderName());
 		setApiKey(vo.getApiKey());
 	}
+	@Override
+	public String toString() {
+		return "ProviderDTO [providerId=" + providerId + ", providerName=" + providerName + ", apiKey=" + apiKey + "]";
+	}
+	public String getProviderId() {
+		return providerId;
+	}
+	public void setProviderId(String providerId) {
+		this.providerId = providerId;
+	}
+	public String getProviderName() {
+		return providerName;
+	}
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+	public String getApiKey() {
+		return apiKey;
+	}
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+	
+	
 }
