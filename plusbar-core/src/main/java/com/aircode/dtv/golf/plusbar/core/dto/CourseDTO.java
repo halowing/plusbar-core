f37a790362a6e9b3205803e8eb1435b3561aae18
김성근 <sgkim@aircode.com>
2022-06-22 12:41:09 +0900

    코스 부가정보 처리를 위해 추가

---
+++ plusbar-core/src/main/java/com/aircode/dtv/golf/plusbar/core/dto/CourseDTO.java
@@ -0,0 +1,530 @@
+package com.aircode.dtv.golf.plusbar.core.dto;
+
+import java.io.Serializable;
+
+import com.aircode.dtv.golf.plusbar.core.vo.CourseVO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.ALWAYS)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CourseDTO implements Serializable{
+	
+	private static final long serialVersionUID = 8418295061055165629L;
+	
+	private String gameId;
+	private String courseId;
+	private String courseName;
+	private Integer holePar1          ;
+	private Integer holePar2          ;
+	private Integer holePar3          ;
+	private Integer holePar4          ;
+	private Integer holePar5          ;
+	private Integer holePar6          ;
+	private Integer holePar7          ;
+	private Integer holePar8          ;
+	private Integer holePar9          ;
+	private Integer holePar10         ;
+	private Integer holePar11         ;
+	private Integer holePar12         ;
+	private Integer holePar13         ;
+	private Integer holePar14         ;
+	private Integer holePar15         ;
+	private Integer holePar16         ;
+	private Integer holePar17         ;
+	private Integer holePar18         ;
+	private Integer holeParIn         ;
+	private Integer holeParOut        ;
+	private Integer holeParSum        ;
+	
+	private String courseImageUrl	;
+	private String holeImageUrl1	;
+	private String holeImageUrl2	;
+	private String holeImageUrl3	;
+	private String holeImageUrl4	;
+	private String holeImageUrl5	;
+	private String holeImageUrl6	;
+	private String holeImageUrl7	;
+	private String holeImageUrl8	;
+	private String holeImageUrl9	;
+	private String holeImageUrl10	;
+	private String holeImageUrl11	;
+	private String holeImageUrl12	;
+	private String holeImageUrl13	;
+	private String holeImageUrl14	;
+	private String holeImageUrl15	;
+	private String holeImageUrl16	;
+	private String holeImageUrl17	;
+	private String holeImageUrl18	;
+	private String holeContent1	    ;
+	private String holeContent2	    ;
+	private String holeContent3	    ;
+	private String holeContent4	    ;
+	private String holeContent5	    ;
+	private String holeContent6	    ;
+	private String holeContent7	    ;
+	private String holeContent8	    ;
+	private String holeContent9	    ;
+	private String holeContent10	;
+	private String holeContent11	;
+	private String holeContent12	;
+	private String holeContent13	;
+	private String holeContent14	;
+	private String holeContent15	;
+	private String holeContent16	;
+	private String holeContent17	;
+	private String holeContent18	;
+	
+	public CourseDTO() {}
+	
+	public CourseDTO(CourseVO vo) {
+		this.gameId             = vo.getGameId();
+		this.courseId           = vo.getCourseId();
+		this.courseName         = vo.getCourseName();
+		this.holePar1           = vo.getHolePar1();
+		this.holePar2           = vo.getHolePar2();  
+		this.holePar3           = vo.getHolePar3();  
+		this.holePar4           = vo.getHolePar4();  
+		this.holePar5           = vo.getHolePar5();  
+		this.holePar6           = vo.getHolePar6();  
+		this.holePar7           = vo.getHolePar7();  
+		this.holePar8           = vo.getHolePar8();  
+		this.holePar9           = vo.getHolePar9();  
+		this.holePar10          = vo.getHolePar10();  
+		this.holePar11          = vo.getHolePar11();  
+		this.holePar12          = vo.getHolePar12();  
+		this.holePar13          = vo.getHolePar13();  
+		this.holePar14          = vo.getHolePar14();  
+		this.holePar15          = vo.getHolePar15();  
+		this.holePar16          = vo.getHolePar16();  
+		this.holePar17          = vo.getHolePar17();
+		this.holePar18          = vo.getHolePar18();
+		this.holeParIn          = vo.getHoleParIn();
+		this.holeParOut         = vo.getHoleParOut();
+		this.holeParSum         = vo.getHoleParSum();
+		this.courseImageUrl	    = vo.getCourseImageUrl();
+		this.holeImageUrl1	    = vo.getHoleImageUrl1();	
+		this.holeImageUrl2	    = vo.getHoleImageUrl2();
+		this.holeImageUrl3	    = vo.getHoleImageUrl3();
+		this.holeImageUrl4	    = vo.getHoleImageUrl4();
+		this.holeImageUrl5	    = vo.getHoleImageUrl5();
+		this.holeImageUrl6	    = vo.getHoleImageUrl6();
+		this.holeImageUrl7	    = vo.getHoleImageUrl7();
+		this.holeImageUrl8	    = vo.getHoleImageUrl8();
+		this.holeImageUrl9	    = vo.getHoleImageUrl9();
+		this.holeImageUrl10	    = vo.getHoleImageUrl10();
+		this.holeImageUrl11	    = vo.getHoleImageUrl11();	
+		this.holeImageUrl12	    = vo.getHoleImageUrl12();	
+		this.holeImageUrl13	    = vo.getHoleImageUrl13();	
+		this.holeImageUrl14	    = vo.getHoleImageUrl14();	
+		this.holeImageUrl15	    = vo.getHoleImageUrl15();	
+		this.holeImageUrl16	    = vo.getHoleImageUrl16();	
+		this.holeImageUrl17	    = vo.getHoleImageUrl17();	
+		this.holeImageUrl18	    = vo.getHoleImageUrl18();	
+		this.holeContent1	    = vo.getHoleContent1();	
+		this.holeContent2	    = vo.getHoleContent2();
+		this.holeContent3	    = vo.getHoleContent3();
+		this.holeContent4	    = vo.getHoleContent4();
+		this.holeContent5	    = vo.getHoleContent5();
+		this.holeContent6	    = vo.getHoleContent6();
+		this.holeContent7	    = vo.getHoleContent7();
+		this.holeContent8	    = vo.getHoleContent8();
+		this.holeContent9	    = vo.getHoleContent9();
+		this.holeContent10	    = vo.getHoleContent10();
+		this.holeContent11	    = vo.getHoleContent11();
+		this.holeContent12	    = vo.getHoleContent12();
+		this.holeContent13	    = vo.getHoleContent13();
+		this.holeContent14	    = vo.getHoleContent14();
+		this.holeContent15	    = vo.getHoleContent15();
+		this.holeContent16	    = vo.getHoleContent16();
+		this.holeContent17	    = vo.getHoleContent17();
+		this.holeContent18	    = vo.getHoleContent18();
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"CourseDTO [gameId=%s, courseId=%s, courseName=%s, holePar1=%s, holePar2=%s, holePar3=%s, holePar4=%s, holePar5=%s, holePar6=%s, holePar7=%s, holePar8=%s, holePar9=%s, holePar10=%s, holePar11=%s, holePar12=%s, holePar13=%s, holePar14=%s, holePar15=%s, holePar16=%s, holePar17=%s, holePar18=%s, holeParIn=%s, holeParOut=%s, holeParSum=%s, courseImageUrl=%s, holeImageUrl1=%s, holeImageUrl2=%s, holeImageUrl3=%s, holeImageUrl4=%s, holeImageUrl5=%s, holeImageUrl6=%s, holeImageUrl7=%s, holeImageUrl8=%s, holeImageUrl9=%s, holeImageUrl10=%s, holeImageUrl11=%s, holeImageUrl12=%s, holeImageUrl13=%s, holeImageUrl14=%s, holeImageUrl15=%s, holeImageUrl16=%s, holeImageUrl17=%s, holeImageUrl18=%s, holeContent1=%s, holeContent2=%s, holeContent3=%s, holeContent4=%s, holeContent5=%s, holeContent6=%s, holeContent7=%s, holeContent8=%s, holeContent9=%s, holeContent10=%s, holeContent11=%s, holeContent12=%s, holeContent13=%s, holeContent14=%s, holeContent15=%s, holeContent16=%s, holeContent17=%s, holeContent18=%s]",
+				gameId, courseId, courseName, holePar1, holePar2, holePar3, holePar4, holePar5, holePar6, holePar7,
+				holePar8, holePar9, holePar10, holePar11, holePar12, holePar13, holePar14, holePar15, holePar16,
+				holePar17, holePar18, holeParIn, holeParOut, holeParSum, courseImageUrl, holeImageUrl1, holeImageUrl2,
+				holeImageUrl3, holeImageUrl4, holeImageUrl5, holeImageUrl6, holeImageUrl7, holeImageUrl8, holeImageUrl9,
+				holeImageUrl10, holeImageUrl11, holeImageUrl12, holeImageUrl13, holeImageUrl14, holeImageUrl15,
+				holeImageUrl16, holeImageUrl17, holeImageUrl18, holeContent1, holeContent2, holeContent3, holeContent4,
+				holeContent5, holeContent6, holeContent7, holeContent8, holeContent9, holeContent10, holeContent11,
+				holeContent12, holeContent13, holeContent14, holeContent15, holeContent16, holeContent17,
+				holeContent18);
+	}
+	public String getGameId() {
+		return gameId;
+	}
+	public String getCourseId() {
+		return courseId;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public Integer getHolePar1() {
+		return holePar1;
+	}
+	public Integer getHolePar2() {
+		return holePar2;
+	}
+	public Integer getHolePar3() {
+		return holePar3;
+	}
+	public Integer getHolePar4() {
+		return holePar4;
+	}
+	public Integer getHolePar5() {
+		return holePar5;
+	}
+	public Integer getHolePar6() {
+		return holePar6;
+	}
+	public Integer getHolePar7() {
+		return holePar7;
+	}
+	public Integer getHolePar8() {
+		return holePar8;
+	}
+	public Integer getHolePar9() {
+		return holePar9;
+	}
+	public Integer getHolePar10() {
+		return holePar10;
+	}
+	public Integer getHolePar11() {
+		return holePar11;
+	}
+	public Integer getHolePar12() {
+		return holePar12;
+	}
+	public Integer getHolePar13() {
+		return holePar13;
+	}
+	public Integer getHolePar14() {
+		return holePar14;
+	}
+	public Integer getHolePar15() {
+		return holePar15;
+	}
+	public Integer getHolePar16() {
+		return holePar16;
+	}
+	public Integer getHolePar17() {
+		return holePar17;
+	}
+	public Integer getHolePar18() {
+		return holePar18;
+	}
+	public Integer getHoleParIn() {
+		return holeParIn;
+	}
+	public Integer getHoleParOut() {
+		return holeParOut;
+	}
+	public Integer getHoleParSum() {
+		return holeParSum;
+	}
+	public String getCourseImageUrl() {
+		return courseImageUrl;
+	}
+	public String getHoleImageUrl1() {
+		return holeImageUrl1;
+	}
+	public String getHoleImageUrl2() {
+		return holeImageUrl2;
+	}
+	public String getHoleImageUrl3() {
+		return holeImageUrl3;
+	}
+	public String getHoleImageUrl4() {
+		return holeImageUrl4;
+	}
+	public String getHoleImageUrl5() {
+		return holeImageUrl5;
+	}
+	public String getHoleImageUrl6() {
+		return holeImageUrl6;
+	}
+	public String getHoleImageUrl7() {
+		return holeImageUrl7;
+	}
+	public String getHoleImageUrl8() {
+		return holeImageUrl8;
+	}
+	public String getHoleImageUrl9() {
+		return holeImageUrl9;
+	}
+	public String getHoleImageUrl10() {
+		return holeImageUrl10;
+	}
+	public String getHoleImageUrl11() {
+		return holeImageUrl11;
+	}
+	public String getHoleImageUrl12() {
+		return holeImageUrl12;
+	}
+	public String getHoleImageUrl13() {
+		return holeImageUrl13;
+	}
+	public String getHoleImageUrl14() {
+		return holeImageUrl14;
+	}
+	public String getHoleImageUrl15() {
+		return holeImageUrl15;
+	}
+	public String getHoleImageUrl16() {
+		return holeImageUrl16;
+	}
+	public String getHoleImageUrl17() {
+		return holeImageUrl17;
+	}
+	public String getHoleImageUrl18() {
+		return holeImageUrl18;
+	}
+	public String getHoleContent1() {
+		return holeContent1;
+	}
+	public String getHoleContent2() {
+		return holeContent2;
+	}
+	public String getHoleContent3() {
+		return holeContent3;
+	}
+	public String getHoleContent4() {
+		return holeContent4;
+	}
+	public String getHoleContent5() {
+		return holeContent5;
+	}
+	public String getHoleContent6() {
+		return holeContent6;
+	}
+	public String getHoleContent7() {
+		return holeContent7;
+	}
+	public String getHoleContent8() {
+		return holeContent8;
+	}
+	public String getHoleContent9() {
+		return holeContent9;
+	}
+	public String getHoleContent10() {
+		return holeContent10;
+	}
+	public String getHoleContent11() {
+		return holeContent11;
+	}
+	public String getHoleContent12() {
+		return holeContent12;
+	}
+	public String getHoleContent13() {
+		return holeContent13;
+	}
+	public String getHoleContent14() {
+		return holeContent14;
+	}
+	public String getHoleContent15() {
+		return holeContent15;
+	}
+	public String getHoleContent16() {
+		return holeContent16;
+	}
+	public String getHoleContent17() {
+		return holeContent17;
+	}
+	public String getHoleContent18() {
+		return holeContent18;
+	}
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public void setHolePar1(Integer holePar1) {
+		this.holePar1 = holePar1;
+	}
+	public void setHolePar2(Integer holePar2) {
+		this.holePar2 = holePar2;
+	}
+	public void setHolePar3(Integer holePar3) {
+		this.holePar3 = holePar3;
+	}
+	public void setHolePar4(Integer holePar4) {
+		this.holePar4 = holePar4;
+	}
+	public void setHolePar5(Integer holePar5) {
+		this.holePar5 = holePar5;
+	}
+	public void setHolePar6(Integer holePar6) {
+		this.holePar6 = holePar6;
+	}
+	public void setHolePar7(Integer holePar7) {
+		this.holePar7 = holePar7;
+	}
+	public void setHolePar8(Integer holePar8) {
+		this.holePar8 = holePar8;
+	}
+	public void setHolePar9(Integer holePar9) {
+		this.holePar9 = holePar9;
+	}
+	public void setHolePar10(Integer holePar10) {
+		this.holePar10 = holePar10;
+	}
+	public void setHolePar11(Integer holePar11) {
+		this.holePar11 = holePar11;
+	}
+	public void setHolePar12(Integer holePar12) {
+		this.holePar12 = holePar12;
+	}
+	public void setHolePar13(Integer holePar13) {
+		this.holePar13 = holePar13;
+	}
+	public void setHolePar14(Integer holePar14) {
+		this.holePar14 = holePar14;
+	}
+	public void setHolePar15(Integer holePar15) {
+		this.holePar15 = holePar15;
+	}
+	public void setHolePar16(Integer holePar16) {
+		this.holePar16 = holePar16;
+	}
+	public void setHolePar17(Integer holePar17) {
+		this.holePar17 = holePar17;
+	}
+	public void setHolePar18(Integer holePar18) {
+		this.holePar18 = holePar18;
+	}
+	public void setHoleParIn(Integer holeParIn) {
+		this.holeParIn = holeParIn;
+	}
+	public void setHoleParOut(Integer holeParOut) {
+		this.holeParOut = holeParOut;
+	}
+	public void setHoleParSum(Integer holeParSum) {
+		this.holeParSum = holeParSum;
+	}
+	public void setCourseImageUrl(String courseImageUrl) {
+		this.courseImageUrl = courseImageUrl;
+	}
+	public void setHoleImageUrl1(String holeImageUrl1) {
+		this.holeImageUrl1 = holeImageUrl1;
+	}
+	public void setHoleImageUrl2(String holeImageUrl2) {
+		this.holeImageUrl2 = holeImageUrl2;
+	}
+	public void setHoleImageUrl3(String holeImageUrl3) {
+		this.holeImageUrl3 = holeImageUrl3;
+	}
+	public void setHoleImageUrl4(String holeImageUrl4) {
+		this.holeImageUrl4 = holeImageUrl4;
+	}
+	public void setHoleImageUrl5(String holeImageUrl5) {
+		this.holeImageUrl5 = holeImageUrl5;
+	}
+	public void setHoleImageUrl6(String holeImageUrl6) {
+		this.holeImageUrl6 = holeImageUrl6;
+	}
+	public void setHoleImageUrl7(String holeImageUrl7) {
+		this.holeImageUrl7 = holeImageUrl7;
+	}
+	public void setHoleImageUrl8(String holeImageUrl8) {
+		this.holeImageUrl8 = holeImageUrl8;
+	}
+	public void setHoleImageUrl9(String holeImageUrl9) {
+		this.holeImageUrl9 = holeImageUrl9;
+	}
+	public void setHoleImageUrl10(String holeImageUrl10) {
+		this.holeImageUrl10 = holeImageUrl10;
+	}
+	public void setHoleImageUrl11(String holeImageUrl11) {
+		this.holeImageUrl11 = holeImageUrl11;
+	}
+	public void setHoleImageUrl12(String holeImageUrl12) {
+		this.holeImageUrl12 = holeImageUrl12;
+	}
+	public void setHoleImageUrl13(String holeImageUrl13) {
+		this.holeImageUrl13 = holeImageUrl13;
+	}
+	public void setHoleImageUrl14(String holeImageUrl14) {
+		this.holeImageUrl14 = holeImageUrl14;
+	}
+	public void setHoleImageUrl15(String holeImageUrl15) {
+		this.holeImageUrl15 = holeImageUrl15;
+	}
+	public void setHoleImageUrl16(String holeImageUrl16) {
+		this.holeImageUrl16 = holeImageUrl16;
+	}
+	public void setHoleImageUrl17(String holeImageUrl17) {
+		this.holeImageUrl17 = holeImageUrl17;
+	}
+	public void setHoleImageUrl18(String holeImageUrl18) {
+		this.holeImageUrl18 = holeImageUrl18;
+	}
+	public void setHoleContent1(String holeContent1) {
+		this.holeContent1 = holeContent1;
+	}
+	public void setHoleContent2(String holeContent2) {
+		this.holeContent2 = holeContent2;
+	}
+	public void setHoleContent3(String holeContent3) {
+		this.holeContent3 = holeContent3;
+	}
+	public void setHoleContent4(String holeContent4) {
+		this.holeContent4 = holeContent4;
+	}
+	public void setHoleContent5(String holeContent5) {
+		this.holeContent5 = holeContent5;
+	}
+	public void setHoleContent6(String holeContent6) {
+		this.holeContent6 = holeContent6;
+	}
+	public void setHoleContent7(String holeContent7) {
+		this.holeContent7 = holeContent7;
+	}
+	public void setHoleContent8(String holeContent8) {
+		this.holeContent8 = holeContent8;
+	}
+	public void setHoleContent9(String holeContent9) {
+		this.holeContent9 = holeContent9;
+	}
+	public void setHoleContent10(String holeContent10) {
+		this.holeContent10 = holeContent10;
+	}
+	public void setHoleContent11(String holeContent11) {
+		this.holeContent11 = holeContent11;
+	}
+	public void setHoleContent12(String holeContent12) {
+		this.holeContent12 = holeContent12;
+	}
+	public void setHoleContent13(String holeContent13) {
+		this.holeContent13 = holeContent13;
+	}
+	public void setHoleContent14(String holeContent14) {
+		this.holeContent14 = holeContent14;
+	}
+	public void setHoleContent15(String holeContent15) {
+		this.holeContent15 = holeContent15;
+	}
+	public void setHoleContent16(String holeContent16) {
+		this.holeContent16 = holeContent16;
+	}
+	public void setHoleContent17(String holeContent17) {
+		this.holeContent17 = holeContent17;
+	}
+	public void setHoleContent18(String holeContent18) {
+		this.holeContent18 = holeContent18;
+	}
+	
+	
+
+}
